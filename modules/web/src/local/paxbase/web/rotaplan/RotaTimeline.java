bbef20917ba013faed1f5dbf0f0a802408d7d22c
derWilkens <github@derWilkens.de>
2017-10-19 23:01:27 +0200

    Timeline Cmp added

---
+++ modules/web/src/local/paxbase/web/rotaplan/RotaTimeline.java
@@ -0,0 +1,243 @@
+package local.paxbase.web.rotaplan;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.inject.Inject;
+
+import com.haulmont.cuba.core.global.DataManager;
+import com.haulmont.cuba.core.global.LoadContext;
+import com.haulmont.cuba.gui.components.AbstractWindow;
+import com.haulmont.cuba.gui.components.OptionsGroup;
+import com.haulmont.cuba.gui.components.PopupView;
+import com.haulmont.cuba.gui.components.ScrollBoxLayout;
+import com.haulmont.cuba.gui.data.CollectionDatasource;
+import com.haulmont.cuba.gui.data.DataSupplier;
+import com.haulmont.cuba.gui.data.Datasource;
+import com.haulmont.cuba.web.gui.components.WebComponentsHelper;
+import com.vaadin.ui.Layout;
+
+import elemental.json.JsonObject;
+import local.paxbase.entity.DutyPeriod;
+import local.paxbase.entity.OffshoreUser;
+import local.paxbase.entity.UserPreference;
+import local.paxbase.entity.UserPreferencesContext;
+import local.paxbase.entity.coredata.FunctionCategory;
+import local.paxbase.entity.coredata.Site;
+import local.paxbase.entity.dto.TimelineDTO;
+import local.paxbase.service.RotaplanService;
+import local.paxbase.service.TimelineService;
+import local.paxbase.service.UserpreferencesService;
+import local.paxbase.web.toolkit.ui.timelinecomponent.RotaplanComponent;
+import local.paxbase.web.toolkit.ui.timelinecomponent.RotaplanComponent.RotaplandChangeListener;
+
+public class RotaTimeline extends AbstractWindow {
+	
+	@Inject
+	private DataManager dataManager;
+	
+	private RotaplanComponent rotaplan;
+	private TimelineDTO dto;
+	
+	/*UI-Components*/
+	@Inject
+	private PopupView campaignSitePopupView;
+	@Inject
+	private OptionsGroup campaignSiteOptionsGroup;
+	@Inject
+	private ScrollBoxLayout timelineBox;
+
+	/*Services*/
+	@Inject
+	private TimelineService timelineDTOService;
+	@Inject
+	private RotaplanService rotaplanService;
+	@Inject
+	private UserpreferencesService preferencesService;
+	
+
+	
+	@Inject
+	private Datasource<DutyPeriod> dutyPeriodDs;
+	@Inject
+	private CollectionDatasource<DutyPeriod, UUID> dutyPeriodsDs;
+	@Inject
+	private CollectionDatasource<FunctionCategory, UUID> functionCategoriesDs;
+	@Inject
+	private CollectionDatasource<Site, UUID> sitesDs;
+	@Override
+	public void init(Map<String, Object> params) {
+		
+		super.init(params);
+		// JS-UI-Komonente
+		rotaplan = new RotaplanComponent();
+		dto = timelineDTOService.getRotoplanDto();
+		if (dto != null) {
+			rotaplan.addDTO("rotaplan", dto);
+			rotaplan.refresh();
+		}
+		com.vaadin.ui.Layout box = (Layout) WebComponentsHelper.unwrap(timelineBox);
+		// box.setWidth("100%");
+		box.addComponent(rotaplan);
+		rotaplan.setListener(new InnerListener());
+		initCampaignSiteOptionGroup();
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private void initCampaignSiteOptionGroup(){
+		sitesDs.refresh();
+		List<Site> preferredSiteList = new ArrayList<Site>();
+		List<UserPreference> preferredSitesRotaplan = preferencesService
+				.getPreferences(UserPreferencesContext.SiteRotaplan);
+
+		for (UserPreference userPreference : preferredSitesRotaplan) {
+			for (Iterator iterator = sitesDs.getItemIds().iterator(); iterator.hasNext();) {
+				UUID uuid = (UUID) iterator.next();
+				if (userPreference.getEntityUuid().equals(uuid)) {
+					preferredSiteList.add((Site) campaignSiteOptionsGroup.getOptionsDatasource().getItem(uuid));
+				}
+			}
+		}
+		campaignSiteOptionsGroup.setValue(preferredSiteList);
+		campaignSiteOptionsGroup.addValueChangeListener(e -> {
+			String selectedValue = e.getValue() == null ? "0" : String.valueOf(((Collection) e.getValue()).size());
+			showNotification("selected: " + selectedValue, NotificationType.HUMANIZED);
+			LinkedHashSet<Site> currentVal = null;
+			LinkedHashSet<Site> prevVal = null;
+
+			currentVal = (LinkedHashSet<Site>) e.getValue();
+
+			if (e.getPrevValue() instanceof LinkedHashSet) {
+				prevVal = (LinkedHashSet<Site>) e.getPrevValue();
+			}
+			if (prevVal != null) {
+				Site removedSite = getRemovedItem(prevVal, currentVal);
+				if(removedSite != null){
+					preferencesService.deletePreference(UserPreferencesContext.SiteRotaplan, removedSite.getUuid());
+				}
+			}
+			Site addedSite = getAddedItem(prevVal, currentVal);
+
+			if (addedSite != null) {
+				preferencesService.createPreference(UserPreferencesContext.SiteRotaplan, addedSite.getUuid(), null);
+			}
+		});
+	}
+	
+	private Site getAddedItem(LinkedHashSet<Site> prevVal, LinkedHashSet<Site> currentVal) {
+		for (Site site : currentVal) {
+			if (prevVal == null || !prevVal.contains(site)) {
+				return site;
+			}
+		}
+		return null;
+	}
+
+	private Site getRemovedItem(LinkedHashSet<Site> prevVal, LinkedHashSet<Site> currentVal) {
+		for (Site site : prevVal) {
+			if (!currentVal.contains(site)) {
+				return site;
+			}
+		}
+		return null;
+	}
+	
+	public void openSiteCampaingChooser() {
+		campaignSitePopupView.setPopupVisible(true);
+	}
+	
+	class InnerListener implements RotaplandChangeListener {
+
+		@SuppressWarnings("rawtypes")
+		@Override
+		public void itemAdded(JsonObject jsonItem) {
+			if (!jsonItem.getString("content").equals("new item")) {
+				
+				DataSupplier dataservice = dutyPeriodDs.getDataSupplier();
+				DutyPeriod newItem = dataservice.newInstance(dutyPeriodDs.getMetaClass());
+
+				//dutyPeriodsTable.setSelected(Collections.emptyList());
+				dutyPeriodDs.setItem((DutyPeriod) newItem);
+				//refreshOptionsForLookupFields();
+				//enableEditControls(true);
+
+				try {
+					newItem.setStart(jsonDateToDate(jsonItem.getString("start")));
+					newItem.setEnd(jsonDateToDate(jsonItem.getString("end")));
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				String userUuid = jsonItem.getString("group");
+				LoadContext<OffshoreUser> loadContext = LoadContext.create(OffshoreUser.class)
+						.setId(UUID.fromString(userUuid)).setView("offshoreUser-browser-view");
+				newItem.setPersonOnDuty(dataManager.load(loadContext));
+
+				Collection items = functionCategoriesDs.getItems();
+				String content = jsonItem.getString("content");
+				
+				Site site = rotaplanService.getSiteByItemDesignation(content);
+				if(site != null){
+					newItem.setSite(site);
+					//newItem.setFunctionCategory(functionCategoriesDs.get );
+				} else {
+					for (Iterator iterator = items.iterator(); iterator.hasNext();) {
+						FunctionCategory cat = (FunctionCategory) iterator.next();
+						if (cat.getCategoryName().equals(content)) {
+							newItem.setFunctionCategory(cat);
+							break;
+						}
+					}
+				}
+				dutyPeriodsDs.updateItem(newItem);
+
+				rotaplan.addDTO("rotaplan", timelineDTOService.getRotoplanDto());
+				rotaplan.refresh();
+			}
+		}
+
+		@Override
+		public void itemMoved(JsonObject jsonItem) {
+			
+			DutyPeriod dutyPeriod = dutyPeriodsDs.getItem(UUID.fromString(jsonItem.getString("id")));
+
+			try {
+
+				dutyPeriod.setStart(jsonDateToDate(jsonItem.getString("start")));
+				dutyPeriod.setEnd(jsonDateToDate(jsonItem.getString("end")));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			dutyPeriodDs.setItem(dutyPeriod);
+		}
+
+		@Override
+		public void itemDeleted(JsonObject jsonItem) {
+			DutyPeriod dutyPeriod = dutyPeriodsDs.getItem(UUID.fromString(jsonItem.getString("id")));
+			if (dutyPeriod != null) {
+				dutyPeriodsDs.removeItem(dutyPeriod);
+				getDsContext().commit();
+				rotaplan.addDTO("rotaplan", timelineDTOService.getRotoplanDto());
+				rotaplan.refresh();
+			}
+		}
+		private Date jsonDateToDate(String rawDate) throws ParseException {
+			DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+			Date date;
+			date = format.parse(rawDate);
+			return date;
+		}
+	}
+}
